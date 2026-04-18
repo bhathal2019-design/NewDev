c0575dc44e9772a516da8a65f34afd410e2317d7
Vikram <bhathal2017.com>
2026-04-18 09:11:39 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(name)
 @app.route("/")
 def home():
-return "Hello, World from Flask1"
+return "Hello, World from Flask!"
 if name == "main":
 app.run(host="0.0.0.0", port=5000)
