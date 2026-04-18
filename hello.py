916941c92a84f0e3c17327d2af255342438879f9
bhathal2019-design <bhathal2019@gmail.com>
2026-04-18 09:24:04 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app = Flask(name)
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-return "Hello, World from Flask1"
-if name == "main":
-app.run(host="0.0.0.0", port=5000)
+    return "Hello, World from Flask!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
